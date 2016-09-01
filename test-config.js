1cbe12f5558ae16e84bc975a99d3db9643a03350
jensenzhang <jingxuan.n.zhang@gmail.com>
2016-09-01 15:08:53 +0800

    Fix plugin not hosted on static server
    Signed-off-by: jensenzhang <jingxuan.n.zhang@gmail.com>

--- test-config.js
+++ test-config.js
@@ -12,6 +12,14 @@ module.exports = function(options, optimist) {
     // },
   ];
 
+  whitelist = {
+    /*
+     * Add plugins you want to serve on the static server.
+     * Like the following format:
+     */
+    // "plugin.test": true
+  };
+
   excludes = {
     /*
      * Add plugins you want to disable.
@@ -21,7 +29,23 @@ module.exports = function(options, optimist) {
     // "./plugin.test.disabled/test.disabled": true,
   };
 
+  blacklist = {
+    /*
+     * Add plugins you don't want to serve on the static server.
+     * For example:
+     */
+    // "plugin.test.disabled": true
+  };
+
   plugins = plugins.concat(includes).filter(function (p) {
+    if (p.packagePath === "./c9.ide.server/plugins") {
+      for (plugin in whitelist) {
+        p.whitelist[plugin] = whitelist[plugin];
+      }
+      for (plugin in blacklist) {
+        p.blacklist[plugin] = blacklist[plugin];
+      }
+    }
     return !excludes[p] && !excludes[p.packagePath];
   });
 
