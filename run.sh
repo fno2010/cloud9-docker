ad92a02d07bd8e8d9f2e68becbd54d402d984b0b
jensenzhang <jingxuan.n.zhang@gmail.com>
2016-09-01 09:35:16 +0800

    Fixed issue for tmux and pty.js
    Signed-off-by: jensenzhang <jingxuan.n.zhang@gmail.com>

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ mkdir -p workspace
 sudo docker run -d -p 9000:80 \
     -v $(pwd)/conf/test-config.js:/cloud9/configs/test-config.js \
     -v $(pwd)/conf/client-workspace-test.js:/cloud9/configs/client-workspace-test.js \
-    $(for i in $(ls plugins); do echo "-v \$(pwd)/plugins/$i:/cloud9/plugins/$i"; done) \
+    $(for i in $(ls plugins); do echo "-v $(pwd)/plugins/$i:/cloud9/plugins/$i"; done) \
     -v $(pwd)/workspace/:/workspace/ $USER/cloud9-docker:latest
