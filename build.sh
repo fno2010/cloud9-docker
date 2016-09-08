4931bf6e21807f766dd2d15ea24416a6950d4c4d
jensenzhang <jingxuan.n.zhang@gmail.com>
2016-09-08 17:46:20 +0800

    Convert USER to lowercase
    Signed-off-by: jensenzhang <jingxuan.n.zhang@gmail.com>

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --force-rm=true --tag="$USER/cloud9-docker:latest" .
+docker build --force-rm=true --tag="$(echo $USER | tr '[:upper:]' '[:lower:]')/cloud9-docker:latest" .
--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ docker run -d -p 9000:80 \
     -v $(pwd)/conf/test-config.js:/cloud9/configs/test-config.js \
     -v $(pwd)/conf/client-workspace-test.js:/cloud9/configs/client-workspace-test.js \
     $(for i in $(ls plugins); do echo "-v $(pwd)/plugins/$i:/cloud9/plugins/$i"; done) \
-    -v $(pwd)/workspace/:/workspace/ $USER/cloud9-docker:latest
+    -v $(pwd)/workspace/:/workspace/ $(echo $USER | tr '[:upper:]' '[:lower:]')/cloud9-docker:latest
