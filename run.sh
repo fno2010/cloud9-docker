9704eedc5bde1683075dfb74a67182473659e16d
Kai Gao <emiapwil@gmail.com>
2016-11-14 17:29:45 -0800

    Change the script to take parameters
    Signed-off-by: Kai Gao <emiapwil@gmail.com>

--- run.sh
+++ run.sh
@@ -13,4 +13,4 @@ docker run -d -p 9000:80 -p 3000:3000 -p 9001-9100:9001-9100 \
     -v $(pwd)/conf/client-workspace-devopen.js:/cloud9/configs/client-workspace-devopen.js \
     -v $(pwd)/conf/devopen-settings.js:/cloud9/settings/devopen.js \
     $(for i in $(ls plugins); do echo "-v $(pwd)/plugins/$i:/cloud9/plugins/$i"; done) \
-    -v $(pwd)/workspace/:/workspace/ devopen-docker
+    -v $(pwd)/workspace/:/workspace/ "$1"
